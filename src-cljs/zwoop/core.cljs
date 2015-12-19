2885869a3241fe9f1b110ba8bf630fafa841b9d5
Henrik <reg@henkelarsson.se>
2015-12-19 19:44:00 +0100

    Added senter router for cljs

--- src-cljs/zwoop/core.cljs
+++ src-cljs/zwoop/core.cljs
@@ -56,7 +56,46 @@
         (.setEnabled true)))
 
 ;; -------------------------
+;; Dispatcher function for sente
+
+(defmulti event-msg-handler :id) ; Dispatch on event-id
+
+;; Wrap for logging, catching, etc.:
+(defn     event-msg-handler* [{:as ev-msg :keys [id ?data event]}]
+  (event-msg-handler ev-msg))
+
+(do ; Client-side methods
+  (defmethod event-msg-handler :default ; Fallback
+    [{:as ev-msg :keys [event]}]
+    (timbre/info "Unhandled event: %s" event))
+
+  (defmethod event-msg-handler :chsk/state
+    [{:as ev-msg :keys [?data]}]
+    (if (= ?data {:first-open? true})
+      (timbre/info "Channel socket successfully established!")
+      (timbre/info "Channel socket state change: %s" ?data)))
+
+  (defmethod event-msg-handler :chsk/recv
+    [{:as ev-msg :keys [?data]}]
+    (timbre/info "Push event from server: %s" ?data))
+
+  (defmethod event-msg-handler :chsk/handshake
+    [{:as ev-msg :keys [?data]}]
+    (let [[?uid ?csrf-token ?handshake-data] ?data]
+      (timbre/info "Handshake: %s" ?data)))
+
+  ;; Add your (defmethod handle-event-msg! <event-id> [ev-msg] <body>)s here...
+  )
 ;; Initialize app
+(def router_ (atom nil))
+
+(defn stop-router! []
+  (when-let [stop-f @router_] (stop-f)))
+
+(defn start-router! []
+  (stop-router!)
+  (reset! router_ (sente/start-chsk-router! ch-chsk event-msg-handler*)))
+
 (defn mount-components []
   (reagent/render [#'page] (.getElementById js/document "app")))
 
@@ -72,5 +111,6 @@
 
 (defn init! []
   (keyboard-events)
+  (start-router!)
   (hook-browser-navigation!)
   (mount-components))
